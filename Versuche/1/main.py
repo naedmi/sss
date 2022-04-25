022de937009aadda9d8d67cfd546d25f3e9336cd
naedmi <namied@pm.me>
2022-04-25 15:51:00 +0200

    add versuch 1: aufgabe 3

--- Versuche/1/main.py
+++ Versuche/1/main.py
@@ -1,6 +1,9 @@
+import math
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy import stats
+
+global a, b
 
 # distance values
 x = [10, 13, 16, 19, 22, 25, 28, 31, 34, 37, 40, 43, 46, 49, 52, 55, 58, 61, 64, 67, 70]
@@ -19,23 +22,30 @@ def read(path):
                          dtype=np.double, skip_header=300, skip_footer=50, usecols=1)  # how many entries?
 
 
+# umkehrung
+def f(v):
+    return np.e ** b * v ** a
+
+
 if __name__ == '__main__':
     # read data
     z = []
     std = []
     for num in range(10, 73, 3):
-        values = read("data/" + str(num) + ".csv")
+        values = read(f'data/{num}.csv')
         z.append(np.mean(values))
         std.append(np.std(values))
 
     # calculate logarithms
-    logx = list(map(lambda val: np.log(val), x))
-    logy = list(map(lambda val: np.log(val), y))
+    log_x, log_y = np.log(x), np.log(y)
 
     # calculate linear regression
-    result = stats.linregress(logx, logy)
-    x1 = np.linspace(np.min(logx), np.max(logx), 21)
+    result = stats.linregress(log_x, log_y)
+    x1 = np.linspace(np.min(log_x), np.max(log_x), 21)
+    a = result.slope
+    b = result.intercept
     y1 = result.slope * x1 + result.intercept  # slope -> gradient
+
 
     # 1st plot - characteristic curve
     plt.title('Kennlinie')
@@ -47,20 +57,20 @@ if __name__ == '__main__':
     plt.savefig('plots/characteristic.png')
     plt.show()
 
-    # 2nd plot - standard deviation
-    plt.title('Standardabweichung')
-    plt.xlabel('Abstand [cm]')
-    plt.ylabel('Spannung [V]')
-    plt.plot(x, std)
-    plt.axis([7, 73, 0, 0.1])
-    plt.savefig('plots/std.png')
-    plt.show()
+    # # 2nd plot - standard deviation
+    # plt.title('Standardabweichung')
+    # plt.xlabel('Abstand [cm]')
+    # plt.ylabel('Spannung [V]')
+    # plt.plot(x, std)
+    # plt.axis([7, 73, 0, 0.1])
+    # plt.savefig('plots/std.png')
+    # plt.show()
 
     # 3rd plot - logarithm
     plt.title('Logarithmierte Werte')
     plt.xlabel('(logarithmierter) Abstand [cm]')
     plt.ylabel('(logarithmierte) Spannung [V]')
-    plt.plot(logx, logy)
+    plt.plot(log_x, log_y)
     plt.savefig('plots/log.png')
     plt.show()
 
@@ -68,9 +78,39 @@ if __name__ == '__main__':
     plt.title('Lineare Regression - Ausgleichsgerade')
     plt.xlabel('(logarithmierter) Abstand [cm]')
     plt.ylabel('(logarithmierte) Spannung [V]')
-    plt.plot(logx, logy, 'ob')
+    plt.plot(log_x, log_y, 'ob')
     plt.plot(x1, y1, '-r')
     plt.savefig('plots/linear-regression.png')
     plt.show()
 
+    print("Nichtlineare Kennlinie: y = e^%.3f * x^%.3f\n" % (result.intercept, result.slope))
 
+    a4_length = read(f'data/a4-lang.csv')
+    a4_length_mean = np.mean(a4_length)
+    a4_width = read(f'data/a4-breit.csv')
+    a4_width_mean = np.mean(a4_width)
+
+    std = np.std(a4_length) / len(a4_length)
+    print(f'std = {std}\n')
+    print(f'68.26%: result is between {a4_length_mean - std} V and {a4_length_mean + std} V')
+    print(f'95%: result is between {a4_length_mean - 1.96 * std} V and {a4_length_mean + 1.96 * std} V\n')
+
+    length_err = result.slope * np.e ** result.intercept * a4_length_mean ** (result.slope - 1) * std
+    print(f'err = {-length_err}\ndist = {f(a4_length_mean)}cm\n')
+
+    print('---------------------------------------------------------------------------------------')
+
+    std = np.std(a4_width) / len(a4_width)
+    print(f'std = {std}\n')
+    print(f'68.26%: result is between {a4_width_mean - std} V and {a4_width_mean + std} V')
+    print(f'95%: result is between {a4_width_mean - 1.96 * std} V and {a4_width_mean + 1.96 * std} V\n')
+
+    width_err = result.slope * np.e ** result.intercept * a4_width_mean ** (result.slope - 1) * std
+    print(f'err = {-width_err}\ndist = {f(a4_width_mean)}cm\n')
+
+    print('---------------------------------------------------------------------------------------')
+
+    length_dist = f(a4_length_mean)
+    width_dist = f(a4_width_mean)
+    print("Fläche: x = %f +- %f [cm^2]" % (length_dist * width_dist, math.sqrt((length_dist * length_err) ** 2
+                                                                               + (width_dist * width_err) ** 2)))
