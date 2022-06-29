1653c13941f17c3746246459a52209bc6d70ccc0
naedmi <namied@pm.me>
2022-06-29 09:40:47 +0200

    use other measurements

--- Versuche/3/2.2.py
+++ Versuche/3/2.2.py
@@ -3,23 +3,21 @@ import numpy as np
 import data.measurements as data
 
 if __name__ == '__main__':
-    # _amplitudeLarge = [20 * np.log10(1 / data.amplitude_big[x])
-    _amplitudeLarge = [20 * np.log10(0.001 * data.amplitude_big[x]/data.peak_b_big[x])
-                       for x in range(0, len(data.amplitude_big))]
+    _amplitude_big = [20 * np.log10(1 / data.amplitude_big[x])
+                      for x in range(0, len(data.amplitude_big))]
 
-    # _amplitudeSmall = [20 * np.log10(1 / data.amplitude_small[x])
-    _amplitudeSmall = [20 * np.log10(0.001 * data.amplitude_small[x]/data.peak_b_small[x])
-                       for x in range(0, len(data.amplitude_small))]
+    _amplitude_small = [20 * np.log10(1 / data.amplitude_small[x])
+                        for x in range(0, len(data.amplitude_small))]
 
     # −∆t * f * 360
-    _phaseShiftLarge = [(data.phase_shift_big[x]/1000000 * -1) * data.freq[x] * 360
+    _phase_shift_big = [(data.phase_shift_big[x] / 1000000 * -1) * data.freq[x] * 360
                         for x in range(0, len(data.phase_shift_big))]
 
-    _phaseShiftSmall = [(data.phase_shift_small[x]/1000000 * -1) * data.freq[x] * 360
-                        for x in range(0, len(data.phase_shift_small))]
+    _phase_shift_small = [(data.phase_shift_small[x] / 1000000 * -1) * data.freq[x] * 360
+                          for x in range(0, len(data.phase_shift_small))]
 
-    plt.plot(data.freq, _amplitudeSmall, 'b', label='small speaker')
-    plt.plot(data.freq, _amplitudeLarge, 'y', label='big speaker')
+    plt.plot(data.freq, _amplitude_small, 'b', label='small speaker')
+    plt.plot(data.freq, _amplitude_big, 'y', label='big speaker')
     plt.title('Bode Amplitude')
     plt.ylabel('amplitude in dB')
     plt.xlabel('frequency in Hz')
@@ -29,8 +27,8 @@ if __name__ == '__main__':
     plt.savefig('plots/bode_amplitude-frequency.png')
     plt.show()
 
-    plt.plot(data.freq, _phaseShiftSmall, 'b', label='small speaker')
-    plt.plot(data.freq, _phaseShiftLarge, 'y', label='big speaker')
+    plt.plot(data.freq, _phase_shift_small, 'b', label='small speaker')
+    plt.plot(data.freq, _phase_shift_big, 'y', label='big speaker')
     plt.title('Bode Phase-Shift')
     plt.ylabel('phase shift in °')
     plt.xlabel('frequency in Hz')
